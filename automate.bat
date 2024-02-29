312a305688c15062516d82276c0217e4d2ad5400
omar <omara569@gmail.com>
2024-02-28 21:56:20 -0600

    Data Update

--- automate.bat
+++ automate.bat
@@ -1,7 +1,10 @@
 ECHO OFF
+ECHO Activating virtual environment
+mma_nlp\Scripts\activate.bat
 ECHO Scraping Script
 python processing\scraper.py
 ECHO Parsing Script
 python processing\parser.py
 ECHO Performing Sentiment Analysis
 python sentiment_analysis\sentiment_analysis.py
+exit
